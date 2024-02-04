5d12e504c0303e9c1871228d76c42df33e334fe8
Andrew Vozniuk <ipz224_vav@student.ztu.edu.ua>
2024-02-04 14:23:09 +0200

    Task "Sum of two numbers" has been finished
     - Everything is working.
     - Nothing to change.

---
+++ Practice/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double sum = a + b;
+            
+            Console.WriteLine("Sum = " + Math.Round(sum, 2));
+        }
+    }
+}
